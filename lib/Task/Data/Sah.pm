87ac6c9810f9eabab5375d8ced1d26007868751a
Steven Haryanto <stevenharyanto@gmail.com>
2012-08-01 06:46:01 +0000

    Import of SHARYANTO/Task-Data-Sah-0.01 from CPAN.
    gitpan-cpan-distribution: Task-Data-Sah
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         SHARYANTO/Task-Data-Sah-0.01.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

---
+++ lib/Task/Data/Sah.pm
@@ -0,0 +1,44 @@
+package Task::Data::Sah;
+
+our $VERSION = '0.01'; # VERSION
+
+1;
+# ABSTRACT: Install all Data::Sah modules
+
+
+__END__
+=pod
+
+=head1 NAME
+
+Task::Data::Sah - Install all Data::Sah modules
+
+=head1 VERSION
+
+version 0.01
+
+=head1 DESCRIPTION
+
+This task will install all Data::Sah modules.
+
+=head1 TASK CONTENTS
+
+=head2 Included modules
+
+=head3 L<Data::Sah>
+
+=head3 L<Sah>
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2012 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
